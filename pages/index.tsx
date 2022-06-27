6288b945f240b35763023c6bc71405d19b291cf2
ChanHyeok Im <imchrg1@gmail.com>
2022-06-27 22:35:15 +0900

    Feature: mui 적용 테스트

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import { Stack, Button } from '@mui/material'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
@@ -13,6 +14,12 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={styles.main}>
+        <Stack spacing={2} direction="row">
+          <Button variant="text">Text</Button>
+          <Button variant="contained">Contained</Button>
+          <Button variant="outlined">Outlined</Button>
+        </Stack>
+
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
