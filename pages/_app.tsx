4fcc87f75c1a807bb923ee1ad2972280744a9a0b
chan <imchrg1@gmail.com>
2022-07-06 15:20:42 +0900

    Modify: theme 관련 코드 삭제

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,9 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import { ThemeProvider } from '@mui/material';
-import { theme } from '../themes/test';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    // <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    // </ThemeProvider>
+    <Component {...pageProps} />
   );
 }
 
