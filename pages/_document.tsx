de90bf2858b3a4fea163c5dba27c3f78415be0df
chan <imchrg1@gmail.com>
2022-07-06 15:22:03 +0900

    Modify: title 태그 관련 코드 위치 이동

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <title>Mongo Admin</title>
         <meta name="description" content="Admin for MongoDB" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" as="style" href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.4/dist/web/static/pretendard-dynamic-subset.css" />
