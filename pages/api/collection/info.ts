6fec4164c03a2904404241b2764b7b3c45cc2233
chan <imchrg1@gmail.com>
2022-07-06 16:03:00 +0900

    Feature: collection 정보 받아오는 라우터 구현

---
+++ pages/api/collection/info.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { MongoClient, Document, WithId } from 'mongodb';
+
+type Data = {
+  success: boolean,
+  collectionStats: Document,
+  documents: WithId<Document>[],
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const { uri, database, collection } = req.body;
+  const client = await MongoClient.connect(uri);
+  const connect = await client.db(database).collection(collection);
+  const collectionStats = await connect.stats();
+  const documents = await connect.find({}).toArray();
+
+  console.log(documents);
+
+  res.status(200).json({ success: true, collectionStats, documents });
+}
