0cb1b765f904af3fc94efb74648d193e26f542c7
chan <imchrg1@gmail.com>
2022-07-06 17:16:57 +0900

    Feature: colleciton 페이지의 document list 컴포넌트 구현

--- pages/databases/[database]/[collection]/index.tsx
+++ pages/databases/[database]/[collection]/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import { Backdrop, CircularProgress, Box, Stack, Typography, Link, Button } from '@mui/material';
-import DeleteIcon from '@mui/icons-material/Delete';
+import {
+  Backdrop,
+  CircularProgress,
+  Box,
+  Stack,
+  Typography,
+  Link,
+  Button,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material';
 import LogoutIcon from '@mui/icons-material/Logout';
 import * as cookie from '../../../../libs/cookie';
 import styles from '../../../../styles/Collection.module.css';
@@ -11,7 +24,7 @@ const Collection: NextPage = () => {
   const [uri, setUri] = React.useState<string>('');
   const [loading, setLoading] = React.useState<boolean>(false);
   const [collectionStats, setCollectionStats] = React.useState<any>({});
-  const [documents, setDocuments] = React.useState<any[]>([]);
+  const [documents, setDocuments] = React.useState<any[]>([0]);
   const router = useRouter();
   const { database, collection } = router.query;
 
@@ -47,10 +60,10 @@ const Collection: NextPage = () => {
         setCollectionStats(data.collectionStats);
         setDocuments(data.documents);
       } else {
-        // router.replace('/connect');  
+        router.replace('/connect');  
       }
     } catch (err) {
-      // router.replace('/connect');
+      router.replace('/connect');
     } finally {
       setLoading(false);
     }
@@ -66,16 +79,58 @@ const Collection: NextPage = () => {
     e.preventDefault();
   };
 
-  const Documents = documents.map((document: any, index: number) => {
+  const Documents = (() => {
+    const DocumentsTableHead = (() => {
+      const TableHeadRows = Object.keys(documents?.[0]).map((key: string, index: number) => {
+        return (
+          <TableCell key={key}>
+            {key}
+          </TableCell>
+        );
+      });
+  
+      return (
+        <TableHead>
+          <TableRow>
+            {TableHeadRows}
+          </TableRow>
+        </TableHead>
+      );
+    })();
+  
+    const DocumentsTableBody = (() => {
+      const TableBodyRows = documents.map((document: any, index: number) => {
+        const TableBodyRowCells = Object.keys(document).map((key: string) => {
+          return (
+            <TableCell key={`${key}-${index}`} sx={{ whiteSpace: 'break-spaces', wordWrap: 'break-word' }}>
+              {typeof document[key] === 'object' ? JSON.stringify(document[key], null, 2) : document[key]}
+            </TableCell>
+          );
+        });
+  
+        return (
+          <TableRow key={`document-${index}`}>
+            {TableBodyRowCells}
+          </TableRow>
+        );
+      });
+  
+      return (
+        <TableBody>
+          {TableBodyRows}
+        </TableBody>
+      );
+    })();
+
     return (
-      <Link key={document.name} href={`${router.asPath}/${document.name}`} underline="hover">
-        <Stack spacing={4} p={2} direction="row" alignItems="center" justifyContent="space-between" sx={{ backgroundColor: '#FAFAFA' }}>
-          <Box>{document.name}</Box>
-          <Button variant="contained" color="error" onClick={onClickDelete}><DeleteIcon /></Button>
-        </Stack>
-      </Link>
+      <TableContainer sx={{ backgroundColor: '#FAFAFA' }}>
+        <Table aria-label="simple table">
+          {DocumentsTableHead}
+          {DocumentsTableBody}
+        </Table>
+      </TableContainer>
     );
-  });
+  })();
 
   const CollectionStats = Object.keys(collectionStats).map((key: string, index: number) => {
     if(typeof collectionStats[key] !== 'object') {
@@ -98,7 +153,7 @@ const Collection: NextPage = () => {
       </header>
 
       <main className={styles.main}>
-        {database ? (
+        {database && collection ? (
           <Stack spacing={4}>
             <Box sx={{ px: 6, py: 2, textAlign: 'center', backgroundColor: '#FAFAFA' }}>
               {`Collection Name: ${collection}`}
