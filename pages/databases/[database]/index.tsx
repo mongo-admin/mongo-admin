bdba3ba531f9d5e60b8034312dc891b1bb6baa9b
chan <imchrg1@gmail.com>
2022-07-06 10:36:22 +0900

    Feature: collection 페이지 파일 및 관련 라우팅 기능 추가

--- pages/databases/[database]/index.tsx
+++ pages/databases/[database]/index.tsx
@@ -6,14 +6,14 @@ import Image from 'next/image';
 import { Backdrop, CircularProgress, Box, Stack, Grid, Typography, Link, Button } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
 import LogoutIcon from '@mui/icons-material/Logout';
-import * as cookie from '../../libs/cookie';
-import styles from '../../styles/Databases.module.css';
+import * as cookie from '../../../libs/cookie';
+import styles from '../../../styles/Databases.module.css';
 
 const Databases: NextPage = () => {
   const [uri, setUri] = React.useState<string>('');
   const [loading, setLoading] = React.useState<boolean>(false);
-  const [databaseInfo, setDatabaseInfo] = React.useState<any>(null);
   const router = useRouter();
+  const { database } = router.query;
 
   React.useEffect(() => {
     if(!cookie.check('CONNECTION-URI')) {
@@ -25,29 +25,15 @@ const Databases: NextPage = () => {
 
   React.useEffect(() => {
     if(uri?.length) {
-      requestDatabaseList();
+      requestCollectionList(database);
     }
   }, [uri]);
 
-  const requestDatabaseList = async () => {
+  const requestCollectionList = async (dbName: string | string[] | undefined) => {
     setLoading(true);
 
     try {
-      const res = await fetch('/api/databases', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ uri }),
-      });
-
-      if(res.status === 200) {
-        const data = await res.json();
-
-        setDatabaseInfo(data.databases);
-      } else {
-        router.replace('/connect');  
-      }
+      
     } catch (err) {
       router.replace('/connect');
     } finally {
@@ -65,17 +51,6 @@ const Databases: NextPage = () => {
     e.preventDefault();
   };
 
-  const Databases = databaseInfo?.databases?.map((item: any, index: number) => {
-    return (
-      <Link key={item.name} my={0.5} href={`${router.pathname}/${item.name}`} underline="hover">
-        <Stack spacing={4} p={2} direction="row" alignItems="center" justifyContent="space-between" sx={{ backgroundColor: '#FAFAFA' }}>
-          <Box>{`${item.name} (${item.sizeOnDisk / 1024} KB)`}</Box>
-          <Button variant="contained" color="error" onClick={onClickDelete}><DeleteIcon /></Button>
-        </Stack>
-      </Link>
-    );
-  });
-
   return (
     <div className={styles.container}>
       <Head>
@@ -92,33 +67,11 @@ const Databases: NextPage = () => {
       </header>
 
       <main className={styles.main}>
-        {databaseInfo ? (
-          <Stack>
-            <Box sx={{ px: 6, py: 2, backgroundColor: '#FAFAFA' }}>
-              {`Connected URI: ${uri}`}
-            </Box>
-            <Grid container spacing={2} mt={2}>
-              <Grid item md={4}>
-                <Box sx={{ px: 2, py: 4, backgroundColor: '#FAFAFA', textAlign: 'center' }}>
-                  {`DB Count: ${databaseInfo?.databases?.length}`}
-                </Box>
-              </Grid>
-              <Grid item md={4}>
-                <Box sx={{ px: 2, py: 4, backgroundColor: '#FAFAFA', textAlign: 'center' }}>
-                  {`Size: ${databaseInfo?.totalSize / 1024} KB`}
-                </Box>
-              </Grid>
-              <Grid item md={4}>
-                <Box sx={{ px: 2, py: 4, backgroundColor: '#FAFAFA', textAlign: 'center' }}>
-                  {`Size: ${databaseInfo?.totalSizeMb} MB`}
-                </Box>
-              </Grid>
-            </Grid>
-            <Stack mt={4}>
-              {Databases}
-            </Stack>
-          </Stack>
-        ) : null}
+        <Stack>
+          <Box sx={{ px: 6, py: 2, backgroundColor: '#FAFAFA' }}>
+            {`DB Name: ${database}`}
+          </Box>
+        </Stack>
       </main>
 
       <footer className={styles.footer}>
