1af9835d426c24b2a8c9db744232aaed96426583
chan <imchrg1@gmail.com>
2022-07-08 14:03:21 +0900

    Feature: document 제거 라우터 파일 추가 및 기능 구현

---
+++ pages/api/document/delete.ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { MongoClient, ObjectId } from 'mongodb';
+
+type Data = {
+  success: boolean,
+  message?: string,
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  try {
+    const { uri, database, collection, document } = req.body;
+    const client = await MongoClient.connect(uri);
+    const connect = await client.db(database).collection(collection);
+
+    await connect.deleteOne({ _id: new ObjectId(document) });
+
+    res.status(200).json({ success: true });
+  } catch (err: any) {
+    console.error(err);
+    res.status(500).json({ success: false, message: err.message });
+  }
+}
