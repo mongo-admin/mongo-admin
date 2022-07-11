2004038f7faf45862c024a44b34d42c45727327d
chan <imchrg1@gmail.com>
2022-07-11 16:46:04 +0900

    Feature: collection 삭제 라우터 관련 파일 추가 및 기능 구현

---
+++ pages/api/collection/delete.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { MongoClient, Document, WithId } from 'mongodb';
+
+type Data = {
+  success: boolean,
+  message?: string,
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  try {
+    const { uri, database, collection } = req.body;
+    const client = await MongoClient.connect(uri);
+    const connect = await client.db(database).collection(collection);
+
+    await connect.drop();
+
+    client.close();
+
+    res.status(200).json({ success: true });
+  } catch (err: any) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+}
