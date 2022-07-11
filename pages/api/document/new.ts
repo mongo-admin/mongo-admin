318137b620f32f16ed121673b32ec6751ba08539
chan <imchrg1@gmail.com>
2022-07-11 15:22:54 +0900

    Feature: document 생성 라우터 관련 파일 및 기능 추가

---
+++ pages/api/document/new.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { MongoClient } from 'mongodb';
+import * as parser from 'mongodb-query-parser';
+
+type Data = {
+  success: boolean,
+  message?: string,
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  try {
+    const { uri, database, collection, document } = req.body;
+    const parsedDocument = parser(document);
+    const client = await MongoClient.connect(uri);
+    const connect = await client.db(database).collection(collection);
+
+    await connect.insertOne(parsedDocument);
+
+    client.close();
+
+    res.status(200).json({ success: true });
+  } catch (err: any) {
+    console.error(err);
+    res.status(500).json({ success: false, message: err.message });
+  }
+}
