9343ecf5cad1fb05d2e8a2dd6bd46ad756ec105b
chan <imchrg1@gmail.com>
2022-07-12 11:15:25 +0900

    Feature: database 생성 관련 라우터 파일 추가 및 기능 구현

---
+++ pages/api/database/new.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { MongoClient, Document, CollectionInfo } from 'mongodb';
+
+type Data = {
+  success: boolean,
+  message?: string,
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  try {
+    const { uri, database } = req.body;
+    const client = await MongoClient.connect(uri);
+    const connect = await client.db(database);
+    
+    await connect.createCollection('delete_later');
+
+    client.close();
+
+    res.status(200).json({ success: true });
+  } catch (err: any) {
+    res.status(500).json({ success: false, message: err.message });
+  }
+}
