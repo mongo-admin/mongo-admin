8c08a80d9305e958d6440449e0f5452b19cabbf6
chan <imchrg1@gmail.com>
2022-07-22 13:41:47 +0900

    Feature: document find 기능에 value type별 query 생성 로직 추가

--- pages/api/collection/find.ts
+++ pages/api/collection/find.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { MongoClient, Document, WithId } from 'mongodb';
+import { MongoClient, Document, WithId, ObjectId } from 'mongodb';
 
 type Data = {
   success: boolean,
@@ -14,17 +14,34 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   try {
-    const { uri, database, collection, page, rowsPerPage, key, value } = req.body;
+    const { uri, database, collection, page, rowsPerPage, key, value, valueType } = req.body;
     const client = await MongoClient.connect(uri);
     const connect = await client.db(database).collection(collection);
     const collectionStats = await connect.stats();
-    const documentsTotalCount = await connect.countDocuments({[key]: value});
-    const documents = await connect.find({[key]: value}).skip(page * rowsPerPage).limit(rowsPerPage).toArray();
+    let query = {};
+
+    switch(valueType) {
+      case 'string':
+        query = {[key]: value};
+        break;
+      case 'number':
+        query = {[key]: Number(value)};
+        break;
+      case 'objectid':
+        query = {[key]: new ObjectId(value)};
+        break;
+      default:
+        break;
+    }
+
+    const documentsTotalCount = await connect.countDocuments(query);
+    const documents = await connect.find(query).skip(page * rowsPerPage).limit(rowsPerPage).toArray();
 
     client.close();
 
     res.status(200).json({ success: true, collectionStats, documentsTotalCount, documents });
   } catch (err: any) {
+    console.log(err);
     res.status(500).json({ success: false, message: err.message });
   }
 }
